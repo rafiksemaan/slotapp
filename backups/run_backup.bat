aec4fe13c3b2a5055b5a9613e12d51473cf65e18
Rafik Semaan <rafiksemaan@gmail.com>
2025-06-25 08:56:36 +0300

    Update run_backup.bat

--- backups/run_backup.bat
+++ backups/run_backup.bat
@@ -1,5 +1,5 @@
 @echo off
-set BACKUP_DIR=C:\wamp64\backups
+set BACKUP_DIR=C:\wamp64\www\slotapp\backups\db_daily_backups
 set DATE=%date:~-4,4%-%date:~-10,2%-%date:~-7,2%
 set PHP_PATH=C:\wamp64\bin\php\php8.0.26\php.exe
 set SCRIPT_PATH=C:\wamp64\www\slotapp\backups\backup.php
