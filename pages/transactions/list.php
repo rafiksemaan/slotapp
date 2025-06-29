14f31dbc062cc71fed96c3b29e5cf84cb122e0ea
Rafik Semaan <rafiksemaan@gmail.com>
2025-06-29 08:07:35 +0300

    20 transactions load

--- pages/transactions/list.php
+++ pages/transactions/list.php
@@ -6,7 +6,7 @@
 
 // Pagination parameters
 $page_num = 1; // Always start with page 1 for initial load
-$per_page = 5;
+$per_page = 20;
 
 // Sorting parameters
 $sort_column = $_GET['sort'] ?? 'timestamp';
