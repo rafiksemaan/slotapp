f49ed48f4f19a04651c02aa0cd3b5cf266533905
Rafik Semaan <rafiksemaan@gmail.com>
2025-07-04 13:39:12 +0300

    Update delete.php

--- pages/import_transactions/delete.php
+++ pages/import_transactions/delete.php
@@ -26,29 +26,31 @@ try {
     // Start transaction
     $conn->beginTransaction();
 
-    // Get upload details to find the operation_date
+    // Get upload details to find the operation_date and filename
     $upload_stmt = $conn->prepare("SELECT upload_date, upload_filename FROM transaction_uploads WHERE id = ?");
     $upload_stmt->execute([$upload_id]);
     $upload = $upload_stmt->fetch(PDO::FETCH_ASSOC);
 
     if (!$upload) {
-        $conn->rollBack();
-        header("Location: ../../index.php?page=import_transactions&error=Upload record not found");
+        $conn->rollBack(); // If transaction was started
+        header("Location: ../../index.php?page=import_transactions&error=Upload record not found for ID: {$upload_id}");
         exit;
     }
 
     $operation_date_to_delete = $upload['upload_date'];
     $upload_filename = $upload['upload_filename'];
 
+    // Escape '%' and '_' characters in the filename for use in LIKE clause
+    // The ESCAPE '\' clause tells SQL to treat '\' as the escape character.
+    $escaped_filename = str_replace(['%', '_'], ['\\%', '\\_'], $upload_filename);
+
     // Delete associated transactions for this operation_date and filename
-    // This assumes transactions imported from a specific file for a specific date can be uniquely identified.
-    // If multiple files can be imported for the same operation_date, you might need a more robust linking.
-    // For this implementation, we'll rely on the operation_date and the notes field (which contains the filename).
+    // The notes field is set as "Imported from {filename}" during import.
     $delete_transactions_stmt = $conn->prepare("
         DELETE FROM transactions 
-        WHERE operation_date = ? AND notes LIKE ?
+        WHERE operation_date = ? AND notes LIKE ? ESCAPE '\\'
     ");
-    $delete_transactions_stmt->execute([$operation_date_to_delete, "%Imported from {$upload_filename}%"]);
+    $delete_transactions_stmt->execute([$operation_date_to_delete, "Imported from {$escaped_filename}"]);
     $deleted_transactions_count = $delete_transactions_stmt->rowCount();
 
     // Delete the upload record itself
