3c43cd82531791bcdc63873c3d89acdb2c93faaf
Rafik Semaan <rafiksemaan@gmail.com>
2025-07-05 13:35:02 +0300

    Update url_cleaner.js

--- assets/js/url_cleaner.js
+++ assets/js/url_cleaner.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // Use a small timeout to ensure the browser has rendered the message before cleaning the URL
             setTimeout(() => {
                 window.history.replaceState({}, document.title, window.location.pathname + window.location.search.replace(/&?(message|error)=[^&]*/g, ''));
-            }, 100); // A short delay
+            }, 1000); // A short delay
         }
     }
 });
