5d0359854bf78af45de74fd03125acc12bfd8d79
Rafik Semaan <rafiksemaan@gmail.com>
2025-07-07 12:01:00 +0300

    Update edit.php

--- pages/brands/edit.php
+++ pages/brands/edit.php
@@ -38,8 +38,9 @@ try {
 
 // Handle form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $brand['name'] = sanitize_input($_POST['name'] ?? '');
-    $brand['description'] = sanitize_input($_POST['description'] ?? '');
+    $brand['name'] = get_input(INPUT_POST, 'name', 'string');
+	$brand['description'] = get_input(INPUT_POST, 'description', 'string');
+
 
     // Validate required fields
     if (empty($brand['name'])) {
