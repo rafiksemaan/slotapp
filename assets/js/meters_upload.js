13aa6474abf623cffcb0dddf156a5d8e709e15bc
Rafik Semaan <rafiksemaan@gmail.com>
2025-07-08 09:54:44 +0300

    Update meters_upload.js

--- assets/js/meters_upload.js
+++ assets/js/meters_upload.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function sendDataToBackend(uploadDate, filename, meterData) {
-        fetch('pages/meters/upload.php', { // <--- Changed URL here
+        fetch('pages/meters/process_upload.php', { // <--- Changed URL here to the new processing file
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
